88dc55643c194cb6aebae0ac05360e7e2cd712e8
NiutaR <69516148+NiutaR@users.noreply.github.com>
2021-04-11 14:06:18 +0300

    fixed some bugs

---
+++ .history/js/oop_20210411140358.js
@@ -0,0 +1,103 @@
+/*
+class BookList {
+    constructor(markedRead, markedNotReadYet, readBooks, unreadBooks, nextRead, currentRead, lastBook, allBooks) {
+        this.markedRead = 0;
+        this.markedNotReadYet = 0;
+        this.readBooks = [];
+        this.unreadBooks = [];
+        this.nextRead = nextRead;
+        this.currentRead = currentRead;
+        this.lastBook = lastBook;
+        this.allBooks = [];
+    }
+    add(book) {
+        this.allBooks.push(book);
+        this.nextRead = this.allBooks[this.markedRead + 1];
+        this.markedNotReadYet++;
+        
+    }
+    finishCurrentBook() {
+        this.currentRead.readable = true;
+        this.currentRead.readDate = new Date(Date.now());
+        this.lastBook = this.currentRead;
+        this.currentRead = this.nextRead;
+        this.markedRead++;
+        this.markedNotReadYet--;
+        if (this.markedNotReadYet === 1) {
+            this.nextRead = null;
+        } else {
+            this.nextRead = this.allBooks[this.markedRead + 1];
+        }
+    }
+
+    addAll(books){
+        for (let book of books) {
+            this.add(book);
+        }
+    }
+    getByAuthor(author) {
+        return this.allBooks.filter(book => book.author === author);
+    }
+
+    getByGenre(genre) {
+        return this.allBooks.filter(book => book.genre === genre);
+    }
+    getByTitle(title) {
+        return this.allBooks.filter(book => book.title === title);
+    }
+}
+class Book {
+    constructor(title, genre, author, readable = true, readDate) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.readable = readable;
+        this.readDate = readDate;
+    }
+}
+
+const book1 = new Book('Little Women', 'Novel', 'Louisa May Alcott', true,);
+const book2 = new Book('The Adventures of Tom Sawyer', 'Adventures', 'Mark Twain', false);
+const book3 = new Book('Shantaram','Novel', 'Gregory David Roberts', true);
+const book4 = new Book('Good Wives', 'Novel', 'Louisa May Alcott', false)
+const listOfBooks = new BookList();
+listOfBooks.add(book1);
+listOfBooks.add(book2);
+listOfBooks.add(book3);
+listOfBooks.add();
+console.log(listOfBooks);
+*/
+
+class BookList { //Parent Class
+    constructor(markedRead, markedNotReadYet, readBooks, unreadBooks, nextRead, currentRead, lastBook, allBooks) {
+        this.markedRead = 0;
+        this.markedNotReadYet = 0;
+        this.readBooks = [];
+        this.unreadBooks = [];
+        this.nextRead = nextRead;
+        this.currentRead = currentRead;
+        this.lastBook = lastBook;
+        this.allBooks = [];
+    }
+
+    getMarkedRead() {
+        console.log(this.markedRead);
+    }
+    getNotReadYet() {
+        return this.markedNotReadYet.join(',')
+    }
+}
+
+class Book {
+    constructor(title, genre, author, readable, readDate) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.readable = readable;
+        this.readDate = readDate;
+    }
+}
+
+
+
+
---
+++ chat-app/index.js
@@ -0,0 +1,79 @@
+const users = [];
+let enterCount = 0;
+
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('.form-login');
+const passInput = document.querySelector('.form-password');
+const formButton = document.querySelector('.form-btn');
+
+const chatButton = document.querySelector('.chat__btn');
+const chatInput = document.querySelector('.chat__input');
+const chatMessages = document.querySelector('.chat__messages');
+const chatUserName = document.querySelector('.chat__user-name');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
+
+function addUser() {
+  const user = {
+    name: nameInput.value,
+    password: passInput.value,
+    isLogged: true,
+    message: []
+  };
+
+  users.push(user);
+  nameInput.value = '';
+  passInput.value = '';
+
+  localStorage.setItem('users', JSON.stringify(users));
+
+  const getUser = localStorage.getItem('users');
+  const parsedUser = JSON.parse(getUser);
+
+  chatUserName.innerHTML = parsedUser[0].name;
+
+  const { isLogged } = user;
+  if (isLogged) {
+    form.classList.toggle('d-hidden');
+  }
+  enterCount += 1;
+  console.log(enterCount);
+}
+
+formButton.addEventListener('click', addUser);
+
+// localStorage.setItem('users', JSON.stringify(users));
+const getUser = localStorage.getItem('users');
+const parsedUser = JSON.parse(getUser);
+
+if (enterCount !== 0) {
+  chatUserName.innerHTML = parsedUser[0].name;
+}
+
+chatButton.addEventListener('click', () => {
+  const getUsr = localStorage.getItem('users');
+  const parsedUsr = JSON.parse(getUsr);
+
+  console.log(parsedUsr);
+  for (let i = 0; i < parsedUsr.length; i += 1) {
+    parsedUsr[i].message.push(chatInput.value);
+    localStorage.setItem('users', JSON.stringify(parsedUsr));
+  }
+
+  console.log(parsedUsr);
+  const showMessage = localStorage.getItem('users');
+  const parsedMessage = JSON.parse(showMessage);
+
+  console.log(showMessage);
+  console.log(parsedMessage[0].message);
+
+  chatMessages.innerHTML += `<div class="chat__second-user">
+        <span class="chat__message chat__message_second">
+        ${parsedMessage[0].message[parsedMessage[0].message.length - 1]}
+        </span>
+      </div>`;
+
+  chatInput.value = '';
+});
